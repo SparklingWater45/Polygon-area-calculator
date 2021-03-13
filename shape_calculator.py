c946ff385b01733df66555b61e025133372fb254
Dylan van Heerden <dylanvh45@gmail.com>
2021-03-13 13:54:55 +0200

    Complete

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,79 @@
+import math
 class Rectangle:
 
+    def __init__(self,width,height):
+        self.width = width
+        self.height = height
+
+    
+    def set_width(self,width):
+        self.width = width
+    
+    def set_height(self,height):
+        self.height = height
+
+    def get_area(self):
+        return self.width*self.height
+
+    def get_perimeter(self):
+        return (2*self.width+2*self.height)
+
+    def get_diagonal(self):
+        return ((self.width**2+self.height**2)**0.5)
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+
+        output = ''
+        for lines in range(self.height):
+            if output != '':
+                output += '\n'
+            for stars in range(self.width):
+                output += '*'
+        return output + '\n'
+
+
+    def get_amount_inside(self,shape):
+        
+        #If the input shape bigger than shape it's going inside
+
+        print('rectangle = '+ str(self.width + self.height))
+        print('square = ' + str(shape.width + shape.height))
+        
+        if (self.width + self.height) < (shape.width + shape.height):
+            return 0
+        
+        return math.floor((self.width/shape.width)*(self.height/shape.height))
+
+    def __str__(self):
+        return 'Rectangle(width={}, height={})'.format(self.width,self.height)
 
 
 
-class Square:
+class Square(Rectangle):
+
+    def __init__(self,length):
+        super().__init__(length,length)
+
+    def __str__(self):
+        return 'Square(side={})'.format(self.width,self.height)
+
+    def set_side(self,length):
+        return super().set_width(length),super().set_height(length)
+ 
+
+    def set_width(self, length):
+        return super().set_width(length),super().set_height(length)
+    
+    def set_height(self, length):
+        return super().set_height(length),super().set_width(length)
+    
+    def get_area(self):
+        return super().get_area()
+
+    def get_diagonal(self):
+        return super().get_diagonal()
+
+    # def get_amount_inside(self, shape):
+    #     return super().get_amount_inside(shape)
---
+++ test.py
@@ -0,0 +1,7 @@
+
+a = 5
+b = 6
+
+c = max(a,b)
+
+print(c)
